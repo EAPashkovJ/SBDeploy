300c240fb5439fa80586f74f47aa82f74a4f499b
Evgenii <eapashkov@gmail.com>
2022-10-30 13:31:29 +0300

    Initial commit

---
+++ src/main/java/com/example/sbdeploy/SbDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDeployApplication.class, args);
+	}
+
+}
