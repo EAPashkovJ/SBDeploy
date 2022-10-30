11c9f4fec5c87c3adcf17f30fc74ae3e13a44c6d
Evgenii <eapashkov@gmail.com>
2022-10-30 15:50:59 +0300

    test

--- src/test/java/com/example/sbdeploy/SbDeployApplicationTests.java
+++ src/test/java/com/example/sbdeploy/SbDeployApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.sbdeploy;
 
+import com.example.sbdeploy.controller.MainController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbDeployApplicationTests {
+    @Autowired
+    MainController mainController;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void HelloShouldReturnHelloWorld() {
+
+        String str = mainController.Hello();
+        String expStr = "Hello world!";
+
+        Assertions.assertEquals(expStr, str);
+    }
 
 }
