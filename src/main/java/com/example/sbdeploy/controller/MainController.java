b244e4eba61f34c3325b2ca53f82f2f1112817ec
Evgenii <eapashkov@gmail.com>
2022-11-01 16:57:55 +0300

    html page added 1

--- src/main/java/com/example/sbdeploy/controller/MainController.java
+++ src/main/java/com/example/sbdeploy/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.sbdeploy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,7 @@ public class MainController {
         return "Hello again";
     }
 
-    @GetMapping("/page")
+    @RequestMapping("/page")
     public String  htmlret(){
         return "helloPage";
     }
