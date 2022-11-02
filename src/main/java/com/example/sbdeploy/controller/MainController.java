44d2d344296c7b8a9949d84c433a4e959a3ba947
Evgenii <eapashkov@gmail.com>
2022-11-02 10:12:30 +0300

    test refs

--- src/main/java/com/example/sbdeploy/controller/MainController.java
+++ src/main/java/com/example/sbdeploy/controller/MainController.java
@@ -1,12 +1,10 @@
 package com.example.sbdeploy.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class MainController {
 
     @GetMapping("/")
@@ -17,11 +15,5 @@ public class MainController {
     @GetMapping("/hello")
     public String  newHello (){
         return "Hello again";
-    }
+    }}
 
-    @RequestMapping("/page")
-    public String  htmlret(){
-        return "htmlPage";
-    }
-
-}
